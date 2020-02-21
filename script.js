d2aca415d88003c57d0735519921ff8e71fec1b6
Tim Rogers <55721671+tkrogers3@users.noreply.github.com>
2020-02-21 18:25:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,19 +10,20 @@ function countdownClock() {
     let timerDifference = timerEnd - timerStart;
     console.log(timerDifference);
 
-    /* Hours/Minutes/Seconds rounded down to even number to divide by 60 seconds to set interval to update every second  */
+    /* Hours/Minutes/Seconds rounded down to even number to divide time difference by  
+    milliseconds*/
 
-    let hours = Math.floor(timerDifference / 3600000);
+    let hours = Math.floor(timerDifference / 3600000);     //milliseconds per hour
     let milliseconds = hours * 3600000;
-    timerDifference = timerDifference - milliseconds;
-    console.log(timerDifference);
-    let minutes = Math.floor(timerDifference / 60000);
-    milliseconds = minutes * 60000;
-    timerDifference = timerDifference - milliseconds;
-    console.log(timerDifference);
+    timerDifference = timerDifference - milliseconds;   //Remaining time in hours - milliseconds
+  
+    let minutes = Math.floor(timerDifference / 60000);      //milliseconds per minute
+    milliseconds = minutes * 60000;                        
+    timerDifference = timerDifference - milliseconds;        //Remaining time in minutes - milliseconds
+  
 
-    let seconds = Math.floor(timerDifference / 1000);
-    console.log({ hours, minutes, seconds });
+    let seconds = Math.floor(timerDifference / 1000);           //milliseconds per second
+  
 
     /*If statements that set the tens digit in Hours/Seconds/Minutes to 0 if less that 10 */
     if (hours < 10) {
@@ -35,17 +36,15 @@ function countdownClock() {
         seconds = "0" + seconds;
     }
 
-    /* If statement to print to the clock "Times Up!"" when the clock hits zero" */
+    /* If statement to print to the clock "Times Up!"" when the clock hits zero.
+  Else pulls the information for time to the element countdownClock in HTML*/
+    
     if (timerDifference < 0) {
         document.getElementById("countdownClock").innerHTML = "Times Up!";
     }
     else {
         document.getElementById("countdownClock").innerHTML = hours + ":" + minutes + ":" + seconds + " ";
     }
-
-
-    /* Pulling the information for time to the element countdownClock in HTML*/
-
 
 } setInterval(countdownClock, 1000); /* Sets interval to 1000 milliseconds which is 1 second,
     so the timer is counting down every second. */
